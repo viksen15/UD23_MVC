5a1f4449a223b49e690bc4d0814fac7fa55c03dc
viksen <viksen_1234@hotmail.com>
2021-02-09 17:40:45 +0100

    mvc_Ej1

---
+++ mvc_Ej1/mvc/Controller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace mvc
+{
+    class Controller
+    {
+        private ModelCliente model = new ModelCliente();
+        public DataTable MostrarClient()
+        {
+            DataTable tabla = new DataTable();
+            tabla = model.Mostrar();
+            return tabla;
+        }
+        public void InsertarClient(string Nombre, string Apellido, string Direccion, int DNI, DateTime Fecha)
+        {
+            model.Insertar(Nombre, Apellido, Direccion, DNI, Fecha);
+        }
+        public void EditarClient(string Nombre, string Apellido, string Direccion, int DNI, DateTime Fecha, int ID)
+        {
+            model.Editar(Nombre, Apellido, Direccion, DNI, Fecha, ID);
+        }
+        public void EliminarClient(int ID)
+        {
+            model.Eliminar(Convert.ToInt32(ID));
+        }
+    }
+}
